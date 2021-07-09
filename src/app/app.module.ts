96ef1f461c51851df88421dbb39f1cb390626624
Benjamin Ikeh <b.ikeh@yahoo.com>
2021-07-09 15:58:31 +0000

    add material module, change imports and export statement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-
+import { MaterialModule } from './material/material.module';
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +13,7 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
